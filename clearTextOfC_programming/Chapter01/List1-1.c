d2be0e05486838f735eb0aac0cc60db5d6403cf1
ohtagaki <sweetlife0815hiikor@gmail.com>
2020-07-01 23:07:57 +0900

    C言語で足し算と引き算をしてみた

---
+++ clearTextOfC_programming/Chapter01/List1-1.c
@@ -0,0 +1,10 @@
+/* 整数35と49を加えた結果を表示*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    /* printf関数を呼び出し*/
+    printf("%d" , 35 + 49);
+    return 0;
+}
---
+++ clearTextOfC_programming/Chapter01/List1-2.c
@@ -0,0 +1,10 @@
+/* 整数23から68を減じた結果を表示*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("%d", 23 - 68);
+
+    return 0;
+}
