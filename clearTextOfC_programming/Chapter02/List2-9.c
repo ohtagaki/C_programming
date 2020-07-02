7fbd70e7bc7cd03f8750bb233780a58e4bca2668
ohtagaki <sweetlife0815hiikor@gmail.com>
2020-07-03 00:05:06 +0900

    2つの整数値を読み込んで平均値を実数で表示

---
+++ clearTextOfC_programming/Chapter02/List2-9.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a, b;
+    puts("2つの整数を入力してください：\n");
+    printf("整数a:");  scanf("%d", &a);
+    printf("整数b:");  scanf("%d", &b);
+
+    printf("2つの整数の平均値は%fです。\n", (a + b) / 2.0);
+    
+    return 0;
+}
