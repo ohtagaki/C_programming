edd6d32b50f50b35c2d9a12eeffa139023fe72ed
ohtagaki <sweetlife0815hiikor@gmail.com>
2020-07-02 23:05:04 +0900

    複数の変換指定

---
+++ clearTextOfC_programming/Chapter02/List2-3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a, b;
+    puts("2‚Â‚Ì®”‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢");
+    printf("a:");  scanf("%d", &a);
+    printf("b:");  scanf("%d", &b);
+
+    printf("a‚ğb‚ÅŠ„‚é‚Æ¤:%dA‚ ‚Ü‚è%d‚Å‚·B\n", a / b, a % b);
+    
+    return 0;
+}
