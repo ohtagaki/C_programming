d5e1826a7f28cf9bbfefd3e725f84b1a5a00d30a
ohtagaki <sweetlife0815hiikor@gmail.com>
2020-07-02 23:00:59 +0900

    最下位桁の値を求める

---
+++ clearTextOfC_programming/Chapter02/List2-2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int no;
+
+    printf("®”‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢");
+    scanf("%d", &no);
+
+    printf("1‚ÌˆÊ‚Ì”š‚Í%d‚Å‚·B\n", no % 10);
+    
+    return 0;
+}
