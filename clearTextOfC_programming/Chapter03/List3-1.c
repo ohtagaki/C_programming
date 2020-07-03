de0f0310b14b9a8e5b6f432585468554777c994a
ohtagaki <sweetlife0815hiikor@gmail.com>
2020-07-03 22:29:08 +0900

    読み込んだ整数が5で割り切れるかどうかチェック

---
+++ clearTextOfC_programming/Chapter03/List3-1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int num;
+    puts("®”‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢F\n");
+    printf("num");  
+    scanf("%d", &num);
+
+    if(num % 5 != 0)
+        puts("‚»‚Ì”‚Í5‚ÅŠ„‚èØ‚ê‚Ü‚¹‚ñB");
+    
+    return 0;
+}
